985b7d47ab58395c828c941559a3d2e6b9cadb6f
Sudil Lakindu Mallika Arachchi <164470662+sudillakindu@users.noreply.github.com>
2025-01-05 00:09:44 +0530

    update Main view ui
    bugs fix

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,11 @@
 import controller.LoginAndSignUpController;
 import model.LoginAndSignUpModel;
-import view.LoginAndSignUpFormView;
-
-import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import view.LoginAndSignUpView;
 
 public class Main {
     public static void main(String[] args) {
-
-        LoginAndSignUpFormView view = new LoginAndSignUpFormView();
-        LoginAndSignUpModel model = new LoginAndSignUpModel();
-        LoginAndSignUpController controller = new LoginAndSignUpController(view,model);
-
-        // Create a JFrame to display the UI
-        JFrame frame = new JFrame();
-        frame.setUndecorated(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.getContentPane().add(view);
-        frame.pack();
-
-        //frame.setContentPane(loginAndSignUpFormView);
-
-        frame.setSize(1024, 768);
-        frame.setLocationRelativeTo(null);
-        addDragFunctionality(frame);
-        frame.setVisible(true);
-
-    }
-
-    // Method to add drag functionality
-    private static void addDragFunctionality(JFrame frame) {
-        final int[] mousePosition = {0, 0};
-
-        // Add mouse listener to capture the click position
-        frame.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                mousePosition[0] = e.getX();
-                mousePosition[1] = e.getY();
-            }
-        });
-
-        // Add mouse motion listener to move the frame
-        frame.addMouseMotionListener(new MouseAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                int x = e.getXOnScreen() - mousePosition[0];
-                int y = e.getYOnScreen() - mousePosition[1];
-                frame.setLocation(x, y);
-            }
-        });
+        LoginAndSignUpView loginAndSignUpView = new LoginAndSignUpView();
+        LoginAndSignUpModel loginAndSignUpModel = new LoginAndSignUpModel();
+        LoginAndSignUpController loginAndSignUpController = new LoginAndSignUpController(loginAndSignUpView, loginAndSignUpModel);
     }
 }
